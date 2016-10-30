24e23854bb58c2c42f9640d0b5ec20bba90e4060
Victor Aluko <victor.aluko@gmail.com>
2016-10-30 05:22:29 +0200

    implemented spec_test_case 4, Robot can retry to find ball

--- main.py
+++ main.py
@@ -9,14 +9,20 @@ coordinates = Coordinates()
 
 input_key = ""
 current_state = "static"
+detect_attempts = 0
 
 while input_key != "q":
     ball_found = input_key == "b"
     coordinate_data = coordinates.get_coordinates(ball_found)
-    print "Coordinates",coordinate_data
+    print "\nCoordinates",coordinate_data
+
     if coordinate_data['ball'] != -1:
         current_state = "driving"
+        detect_attempts = 0
         drive_controller.drive_to_coordinates(coordinate_data['ball'])
+    else:
+        detect_attempts += 1
+        print "Ball not found on attempt:",detect_attempts
 
     print "Current state:",current_state
     input_key = raw_input("Enter q to break, b to detect ball: ")
