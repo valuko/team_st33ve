ef16943fbc712d198221601dae0a137d70001a10
Victor Aluko <victor.aluko@gmail.com>
2016-10-30 05:17:45 +0200

    spec test case 3, Robot can Drive to ball, implemented

--- drive_controller.py
+++ drive_controller.py
@@ -4,3 +4,9 @@ import time
 class DriveController:
     def __init__(self):
         pass
+
+    @staticmethod
+    def drive_to_coordinates(coordinate_set):
+        target_coordinates = coordinate_set
+        print "Driving to coordinates",target_coordinates
+        return True
--- main.py
+++ main.py
@@ -8,9 +8,15 @@ drive_controller = DriveController()
 coordinates = Coordinates()
 
 input_key = ""
+current_state = "static"
 
 while input_key != "q":
     ball_found = input_key == "b"
     coordinate_data = coordinates.get_coordinates(ball_found)
     print "Coordinates",coordinate_data
+    if coordinate_data['ball'] != -1:
+        current_state = "driving"
+        drive_controller.drive_to_coordinates(coordinate_data['ball'])
+
+    print "Current state:",current_state
     input_key = raw_input("Enter q to break, b to detect ball: ")
