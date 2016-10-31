8bfd182c90625a91697a07e141f6385cd826f1f8
icotfodile <ikechukwu.ofodile@estcube.eu>
2016-10-31 10:04:36 +0200

    Driving action implemented

--- drive_controller.py
+++ drive_controller.py
@@ -2,24 +2,55 @@ import time
 
 
 class DriveController:
-    def __init__(self):
-        pass
+    def __init__(self, motor_controller):
+        self.motor_controller = motor_controller
 
     @staticmethod
-    def drive_to_coordinates(coordinate_set):
-        target_coordinates = coordinate_set
-        print "Driving to coordinates",target_coordinates
-        return True
+    def drive_to_coordinates(self, coordinates):
+
+        print "Driving to coordinates",coordinates
+
+        if coordinates == -1:
+            return
+
+        x = coordinates[0]
+        y = coordinates[1]
+
+        # the closer the ball the smaller the speed
+        if y < 150:
+            speed = 70
+        elif y > 500:
+            speed = 20
+        else:
+            speed = 40
+
+        if x > 340:
+            # print("move right")
+            self.motor_controller.move_left_wheel((speed + 8) * -1)
+            self.motor_controller.move_right_wheel(speed - 8)
+            self.motor_controller.move_back_wheel(8)
+        elif x < 310:
+            # print("move left")
+            self.motor_controller.move_right_wheel(speed + 8)
+            self.motor_controller.move_left_wheel((speed - 8) * -1)
+            self.motor_controller.move_back_wheel(8)
+        else:
+            # print("move straght")
+            self.motor_controller.move_right_wheel(speed + 20)
+            self.motor_controller.move_left_wheel((speed + 20) * -1)
 
     @staticmethod
-    def drive_in_circle(speed):
-        circle_speed = speed
-        print "Driving round in circle now at speed:",circle_speed
-        return True
+    def drive_in_circle(self, multiplier=1):
+        circle_speed = 14 * multiplier
+        print "Driving round in circle now at speed:", circle_speed
+        self.motor_controller.move_left_wheel(circle_speed)
+        self.motor_controller.move_right_wheel(circle_speed)
+        self.motor_controller.move_back_wheel(circle_speed)
 
     @staticmethod
-    def stop():
+    def stop(self):
         print "Stopping now"
-        return True
+        self.motor_controller.stop()
+
 
 
