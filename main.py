3f84326450685bcc3ffc53d41910d3a452a2528b
Victor Aluko <victor.aluko@gmail.com>
2016-11-14 23:11:13 +0200

    Refactored to add the mainboard controller code

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from drive_controller import DriveController
 from motor_controller import MotorController
 from coordinates import Coordinates
 from bolt_settings import BoltSettings
+from referee_controller import RefereeController
 import time
 import numpy as np
 import cv2
+import threading
 
 cap = cv2.VideoCapture(1)
 app_settings = BoltSettings()
@@ -12,6 +14,7 @@ val_dict = app_settings.read_dict()
 motor_controller = MotorController()
 drive_controller = DriveController(motor_controller)
 coordinates = Coordinates(val_dict)
+referee_controller = RefereeController(motor_controller, val_dict)
 
 input_key = ""
 current_state = "static"
@@ -21,53 +24,59 @@ max_attempts = 15
 circle_threshold = 3
 command = ""
 
-while True:
-    try:
-        ret, frame = cap.read()
+try:
+    # Start the referee module
+    td1 = threading.Thread(target=referee_controller.listen)
+    td1.start()
 
-        if not ret:
-            print "Frame not ready for readying"
-            continue
+    while True:
+        if referee_controller.game_status():
+            ret, frame = cap.read()
 
-        ball_found = input_key == "b"
-        coordinate_data = coordinates.get_coordinates(frame)
-        print "\nCoordinates", coordinate_data
+            if not ret:
+                print "Frame not ready for readying"
+                continue
 
-        if coordinate_data['ball'] != -1:
-            # Stop first if its circling to avoid sudden jerked movement
-            # if current_state == "circling":
-            # drive_controller.stop()
+            ball_found = input_key == "b"
+            coordinate_data = coordinates.get_coordinates(frame)
+            print "\nCoordinates", coordinate_data
 
-            detect_attempts = 0
-            command = "drivetoball"
-            current_state = "driving"
-        else:
-            detect_attempts += 1
-            print "Ball not found on attempt:", detect_attempts
-            # Drive in circle till you find ball
-            if detect_attempts == circle_threshold:
-                command = "circle"
-                current_state = "circling"
-            # Drive to goal if after no ball still found after max_attempts, this will be a fail safe
-            if detect_attempts == max_attempts:
-                command = "gohome"
+            if coordinate_data['ball'] != -1:
+                # Stop first if its circling to avoid sudden jerked movement
+                if current_state == "circling":
+                    drive_controller.stop()
+
                 detect_attempts = 0
-                current_state = "going_home"
+                command = "drivetoball"
+                current_state = "driving"
+            else:
+                detect_attempts += 1
+                print "Ball not found on attempt:", detect_attempts
+                # Drive in circle till you find ball
+                if detect_attempts == circle_threshold:
+                    command = "circle"
+                    current_state = "circling"
+                # Drive to goal if after no ball still found after max_attempts, this will be a fail safe
+                if detect_attempts == max_attempts:
+                    command = "gohome"
+                    detect_attempts = 0
+                    current_state = "going_home"
 
-        print "Current state:", current_state
+            print "Current state:", current_state
 
-        if command == "circle":
-            drive_controller.drive_in_circle(circle_speed)
-        elif command == "drivetoball":
-            drive_controller.drive_to_coordinates(coordinate_data['ball'])
-        elif command == "gohome":
-            drive_controller.drive_to_coordinates(coordinate_data['black'])
+            if command == "circle":
+                drive_controller.drive_in_circle(circle_speed)
+            elif command == "drivetoball":
+                drive_controller.drive_to_coordinates(coordinate_data['ball'])
+            elif command == "gohome":
+                drive_controller.drive_to_coordinates(coordinate_data['black'])
 
-        # cv2.imshow('Video', frame)
-        key = cv2.waitKey(1)
-        time.sleep(0.1)
-    except KeyboardInterrupt:
-        #drive_controller.stop()
-        break
+            # cv2.imshow('Video', frame)
+            key = cv2.waitKey(1)
+            time.sleep(0.1)
+
+except KeyboardInterrupt:
+    #shutdown
+    exit()
 
 cap.release()
--- mainboard_controller.py
+++ mainboard_controller.py
@@ -3,8 +3,38 @@ from datetime import datetime
 
 
 class MainBoardController:
+    port = "COM5"
+    baud_rate = 9600
+    ball_catch_cmd = "<bl:1>"
+    ball_release_cmd = "<bl:0>"
+
     def __init__(self):
+        self.mainboard = serial.Serial(self.port, self.baud_rate, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, 3)
         self.is_ball = False
 
     def has_ball(self):
         return self.is_ball
+
+    def dribbler_state(self):
+        self.mainboard.write("d700\n")
+
+    def dribbler_stop(self):
+        self.mainboard.write("d0\n")
+
+    def charge(self):
+        print(str(datetime.now()) + " | charge")
+        self.mainboard.write("j\n")
+
+    def kick(self):
+        print(str(datetime.now()) + " | kick")
+        self.mainboard.write("k\n")
+
+    def detect_ball_catch(self):
+        self.is_ball = False
+        while True:
+            line = self.mainboard.readline().strip()
+            if line == self.ball_catch_cmd:
+                self.is_ball = True
+            if line == self.ball_release_cmd:
+                self.is_ball = False
+
