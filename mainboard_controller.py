38b0e152483881363d64041b7e94d685a9b98c3a
Victor Aluko <victor.aluko@gmail.com>
2016-11-17 17:11:05 +0200

    updated code for charger, kicker and dribbler

--- mainboard_controller.py
+++ mainboard_controller.py
@@ -1,4 +1,5 @@
 import serial
+import time
 from datetime import datetime
 
 
@@ -7,6 +8,7 @@ class MainBoardController:
     baud_rate = 9600
     ball_catch_cmd = "<bl:1>"
     ball_release_cmd = "<bl:0>"
+    dribbler_prefix = 'd'
 
     def __init__(self):
         self.mainboard = serial.Serial(self.port, self.baud_rate, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, 3)
@@ -15,18 +17,26 @@ class MainBoardController:
     def has_ball(self):
         return self.is_ball
 
-    def dribbler_state(self):
-        self.mainboard.write("d700\n")
+    def dribbler_start(self):
+        pf = self.dribbler_prefix
+        self.mainboard.write(pf+"0.1\n")
+        time.sleep(1)
+        self.mainboard.write(pf+"6\n")
 
     def dribbler_stop(self):
-        self.mainboard.write("d0\n")
+        pf = self.dribbler_prefix
+        self.mainboard.write(pf+"0\n")
 
-    def charge(self):
+    def charge_kick(self):
         print(str(datetime.now()) + " | charge")
-        self.mainboard.write("j\n")
+        self.mainboard.write("n\n")
 
     def kick(self):
         print(str(datetime.now()) + " | kick")
+        self.mainboard.write("j\n")
+
+    def release_kick(self):
+        print(str(datetime.now()) + " | release_kick")
         self.mainboard.write("k\n")
 
     def detect_ball_catch(self):
